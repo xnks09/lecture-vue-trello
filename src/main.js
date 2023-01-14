8235caa99c57cc8afcd3e873e4ce1c23102a02b1
nks <xnks@gsitm.com>
2023-01-14 15:12:43 +0900

    해쉬뱅모드 제거

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter)
 
@@ -8,6 +8,7 @@ const Login = { template: '<div>Login Page</div>' }
 const NotFound = { template: '<div>Page not found</div>' }
 
 const router = new VueRouter({
+  mode: 'history', //HashBang 모드에서 history로 변경(URL에 #이 포함되므로)
   routes: [
     {path: '/', component: App},
     {path: '/login', component: Login},
